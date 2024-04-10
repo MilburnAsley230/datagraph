073bbf1554df83afa9d3beeb67c0be8f7588ea17
Yin <doyin@deloitte.com.cn>
2024-04-10 18:09:05 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,7 @@
+const capitalizeFirstLetter = require('./capitalizeFirstLetter')
+const arrayAverage = require('./arrayAverage')
+const reverseString = require('./reverseString')
+
+module.exports = (str) => {
+    return str.toUpperCase();
+  }
